d4f12596bb60ca5d52a211c68e68d8c11bf43bfe
TKomrij <tatiane.b.komrij@gmail.com>
2021-01-29 13:51:05 -0600

    add login and registration

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,18 +1,21 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { Home } from "./components/Home"
 import { MovieList} from "./components/movies/MovieList"
 import { MovieProvider } from "./components/movies/MovieProvider"
+import {  MovieDetail } from "./components/movies/MovieDetails"
 
 export const ApplicationViews = () => {
   return (
       <>
-          {/* Render the location list when http://localhost:3000/ */}
           <MovieProvider>
             <Route exact path="/">
-                <Home />
+                <h3>Movies</h3>
                 <MovieList/>
             </Route>
+
+            <Route exact path="/movies/detail/:movieId(\d+)">
+                    <MovieDetail />
+                </Route>
           </MovieProvider>
       </>
   )
---
+++ src/components/movies/MovieDetails.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from "react"
+import { MovieContext } from "./MovieProvider"
+import { useParams } from 'react-router-dom';
+import "./Movie.css"
+
+export const MovieDetail = () => {
+  const { getMovieById } = useContext(MovieContext)
+
+	const [movie, setMovie] = useState({})
+
+    const { movieId } = useParams();
+
+  useEffect(() => {
+    console.log("useEffect", movieId)
+    getMovieById(movieId)
+    .then((response) => {
+      setMovie(response)
+    })
+    }, [])
+
+    
+   
+  
+
+  return (
+    <section className="movieDetails">
+      <img className="movieDetails__poster" src= {"https://image.tmdb.org/t/p/w500/" + movie.poster_path} width="300" height="400"/>
+      <div className="movieDtailes__title">{movie.title}</div>
+      <h3 className="movieDetails__overview">{movie.overview}</h3>
+      <h3 className="movieDetails__overview">{movie.genre_ids}</h3>
+      {/* <div className="movieDetails__genre">{movie.genre_ids.map(genre => genre.id).join(", ")}</div> */}
+    </section>
+  )
+}
