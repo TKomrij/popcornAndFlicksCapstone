db737d45c7dce5a183d4eaf2fe4bb11f4acd8985
TKomrij <tatiane.b.komrij@gmail.com>
2021-02-07 16:25:47 -0600

    get favorite movies by userId created. Utilized and tested

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import "./Movie.css"
 
-export const MovieCard = ({movie}) => {
+export const MovieCard = ({movie, favoriteList}) => {
 
 
 
@@ -25,6 +25,7 @@ const favoriteButtonClicked = (e) => {
 
 }
 
+
 const [watchLaterState, setWatchLaterState] = useState()
 const watchLaterButtonClicked = (e) => {
     e.preventDefault()
@@ -33,6 +34,14 @@ const watchLaterButtonClicked = (e) => {
 
 // check to see if current movie id is in favorited movies or watch laters
 
+const isFavoritedMovie = () => {
+    const movieIds = favoriteList.map(movie => movie.apiId)
+    return movieIds.includes(movie.id)
+}
+
+if(isFavoritedMovie()) {
+    console.log("this  movie is favorited", movie.id)
+}
 
 // if current movie id is in favorited movies array, then favorite state = active, else = inactive
 
@@ -46,7 +55,7 @@ const posterDiv = {
     <section className="movie">
         <Link className="movie__link "to={`/movies/detail/${movie.id}`}>
             <div style={posterDiv} className="movie__poster">
-                <button onClick={watchLaterButtonClicked} className="poster__button"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path className={watchLaterState} fill="grey" d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/></svg></button>
+                {/* <button onClick={watchLaterButtonClicked} className="poster__button"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path className={watchLaterState} fill="grey" d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/></svg></button> */}
                 <button onClick={favoriteButtonClicked}className="poster__button"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path className={favoriteClass} fill="grey" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg></button>
 
             </div>
